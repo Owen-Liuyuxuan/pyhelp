290b0b439e454e25898089a97f67e6ec99381af6
ramlabserver <yliuhb@connect.ust.hk>
2022-07-11 14:10:09 +0800

    add debug utils for vscode debugging

---
+++ src/pyhelp/debug_utils/pyplot.py
@@ -0,0 +1,105 @@
+"""
+    Wrapper for matplotlib.pyplot for better use in vscode debugging.
+"""
+import matplotlib.pyplot as matplotlibplt
+import numpy as np
+import torch
+
+image_std = np.array([0.229, 0.224, 0.225])
+rgb_mean = np.array([0.485, 0.456, 0.406]),
+
+def tensor2numpy(image):
+    if isinstance(image, torch.Tensor):
+        return image.detach().cpu().numpy()
+    elif isinstance(image, np.ndarray):
+        return image
+    else:
+        raise NotImplementedError
+
+def deal_axis(image:np.ndarray):
+    # if [H, W] just passed through
+    if len(image.shape) == 2:
+        return image.copy()
+
+    # image_should_be batch == 0
+    if len(image.shape) == 4:
+        _image = image[0].copy()
+    else:
+        _image = image.copy()
+    
+    if _image.shape[0] == 1: #[1, H, W]
+        return _image[0] #[H, W]
+    
+    if _image.shape[-1] == 1: #[H, W, 1]
+        return _image[..., 0] #[H,W]
+    
+    if _image.shape[0] == 3: #[3, H, W]
+        return _image.transpose(1, 2, 0) #[H, W, 3]
+    
+    if _image.shape[-1] == 3: #[H, W, 3]
+        return _image
+    
+    # [C, H, W] by default
+    return _image.transpose(1, 2, 0) #[H, W, C]
+    
+    
+def type_agnosis(image:np.ndarray):
+    # commonly used visualize image:
+    # [H, W], single heatmap or depth;
+    # [H, W, 3], rgb image;
+    # [H, W, C], feature maps
+    if len(image.shape) == 2:
+        return "single"
+    if len(image.shape) == 3 and image.shape[-1] == 3:
+        return "rgb"
+    else:
+        return "feature"
+    
+def show_single(image, *args, **kwargs):
+    matplotlibplt.imshow(image, *args, **kwargs)
+
+def show_rgb(image:np.ndarray, **kwargs):
+    if image.dtype == np.uint8:
+        matplotlibplt.imshow(image, **kwargs)
+        return
+    
+    denorm = kwargs.get('denorm', False)
+    if denorm:
+        image = image * image_std + rgb_mean
+    image = image * 255
+    image = np.clip(image, 0, 255)
+    image = image.astype(np.uint8)
+    matplotlibplt.imshow(image, **kwargs)
+    return
+
+def show_feature(image:np.ndarray, **kwargs):
+    normed_image = np.linalg.norm(image, axis=-1)
+    matplotlibplt.imshow(normed_image, **kwargs)
+
+show_dict={
+    "single": show_single,
+    "rgb": show_rgb,
+    "feature": show_feature
+}
+
+def imshow(images, *args, **kwargs):
+    if isinstance(images, list):
+        num_images = len(images)
+        num_cols = int(np.sqrt(num_images + 1))
+        num_rows = int(np.ceil(num_images / num_cols))
+    else:
+        images = [images]
+        num_rows = 1
+        num_cols = 1
+    
+    for i, image in enumerate(images):
+        matplotlibplt.subplot(num_rows, num_cols, i + 1)
+        image = tensor2numpy(image)
+        image = deal_axis(image)
+        image_type = type_agnosis(image)
+        print(f"Debugging image type: {image_type}")
+        show_dict[image_type](image, *args, **kwargs)
+    
+    matplotlibplt.show()
+    matplotlibplt.savefig("debug.png")
+
---
+++ src/pyhelp/debug_utils/timing.py
@@ -0,0 +1,27 @@
+from pytest import RecordedHookCall
+import torch
+import time
+def profiler(func, *args, **kwargs):
+    with torch.autograd.profiler.profile(use_cuda=True, record_shapes=True, with_stack=True) as prof:
+        result = func(*args, **kwargs)
+    print(prof.key_averages().table(sort_by="self_cuda_time_total"))
+    return dict(result=result, prof=prof)
+
+def timer(func, *args, **kwargs):
+    torch.cuda.synchronize()
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    result = func(*args, **kwargs)
+    end.record()
+
+    torch.cuda.synchronize()
+    print(f"{func.__name__} time: {start.elapsed_time(end)}ms")
+    return result
+
+if __name__ == '__main__':
+    output = profiler(torch.min, torch.zeros(10000).cuda())
+    assert output['result'] == torch.zeros(10000).cuda().min()
+
+    output = timer(torch.min, torch.zeros(10000).cuda())
+    assert output == torch.zeros(10000).cuda().min()
